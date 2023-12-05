7da4c6eccbd1c472c7b4d5790ba3e4561ebd4dfd
Katze937 <speg2820@gmail.com>
2023-12-04 19:39:26 -0500

    [Update] Refactored project.
    implemented a more reliable approach to play the do scale.

---
+++ src/main/java/multithreading/Main.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package multithreading;
+
+public class Main {
+
+    private final Object lock = new Object();
+    private volatile int currentNoteIndex = 0;
+    private final String[] notes = {"do.wav", "re.wav", "mi.wav", "fa.wav", "sol.wav", "la.wav", "si.wav", "do-octave.wav"};
+    private FilePlayer player = new FilePlayer();
+
+    public static void main(String[] args) {
+        new Main().playScale();
+    }
+
+    public void playScale() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    while (currentNoteIndex < notes.length) {
+                        if (currentNoteIndex % 2 == 0) {  // t1 plays even indexed notes
+                            playNote(notes[currentNoteIndex]);
+                            currentNoteIndex++;
+                        }
+                        lock.notify();
+                        if (currentNoteIndex < notes.length) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    while (currentNoteIndex < notes.length) {
+                        if (currentNoteIndex % 2 != 0) {  // t2 plays odd indexed notes
+                            playNote(notes[currentNoteIndex]);
+                            currentNoteIndex++;
+                        }
+                        lock.notify();
+                        if (currentNoteIndex < notes.length) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    private void playNote(String note) {
+        player.play("C:\\Users\\isabe\\OneDrive\\桌面\\5100\\Bonus\\sound track\\Sounds\\" + note);
+    }
+}
